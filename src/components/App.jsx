d2d1da4bc68a8309140c0b7f091071bcf8f2af2b
noah milliken <noahmilliken@gmail.com>
2023-08-18 16:43:30 -0400

    formatting styles and layouts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,8 @@ function App() {
 
   return (
     <>
-    <h2>Resume Builder</h2>
-    <div className='project-container'>    
-    
+    <div >    
     <InfoForm />
-   
-  
     </div>
     </>
   )
--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Input from "./Input";
 
 const InfoForm = () => {
-  const [info, setInfo] = useState({});
-  const [schoolFormData, setSchoolFormData] = useState([{schoolName: 'Temple ', degree: 'Geography', finishDate: '12-12-2013'}]);
+  const [info, setInfo] = useState({firstName: 'Seth', lastName: 'Milliken', phone:'207-664-8936', email: 'noahmilliken@gmail.com'});
+  const [schoolFormData, setSchoolFormData] = useState([{schoolName: 'Temple ', degree: 'Geography', finishDate: '12-12-2013', description: 'Honors in area of study focus on GIS'}]);
 
   const [professionalFormData, setProfessionalFormData] = useState([{companyName: '', position: '', dateStarted: '', dateFinished: '', description: ''}])
 
@@ -22,7 +22,7 @@ const InfoForm = () => {
     setProfessionalFormData(updatedFormData)
   }
   const  handleAddEducation = () => {
-    setSchoolFormData([...schoolFormData, {schoolName: '', degree: '', finishDate: ''}])
+    setSchoolFormData([...schoolFormData, {schoolName: '', degree: '', finishDate: '', description: ''}])
   }
   const addProfession = () => {
     setProfessionalFormData([...professionalFormData, {companyName: '', position: '', dateStarted: '', dateFinished: '', description: ''}])
@@ -42,9 +42,10 @@ const InfoForm = () => {
   }
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className='project-container'>
+      <div className="forms-container">
+        <h2>Information</h2>
+        <div className="info-container form-container">
           <Input
             type='text'
             name='firstName'
@@ -70,63 +71,72 @@ const InfoForm = () => {
             onChange={handleInfoChange}
           />
         </div>
-        <div>
-          <h1>Education</h1>
-          {schoolFormData.map((data, index)=> (
-            <div key={index}>
-              <label  htmlFor='schoolName'>School Name</label>
-              <input type="text"   placeholder="University of React.js" name="schoolName" value={data.schoolName || ''} onChange={(e) => handleSchoolFormChange(index, e)} />
-              <label  htmlFor='degree'>Degree</label>
-              <input type="text"   placeholder="Masters in React" name="degree" value={data.degree || ''} onChange={(e) => handleSchoolFormChange(index, e)} />
-              <label  htmlFor='date'>Year Completed</label>
-              <input type="date" name="date"  value={data.finisheDate || 'date'}  onChange={(e) => handleSchoolFormChange(index, e)}/>
-              <div>delete<button onClick={ () => handleDeleteEducation(index)}>-</button></div>
-            </div>
-          ))}
-        </div>
-        <div><h3>Add more education:</h3><button onClick={handleAddEducation}>+</button></div>
+        <div className="education-container form-container">
+          <div className="">
+          <h3>Education</h3>
+            {schoolFormData.map((data, index)=> (
+              <form key={index}>
+                {/* School Name */}
+                <label  htmlFor='schoolName'>School Name</label>
+                <input type="text"   placeholder="University of React.js" name="schoolName" value={data.schoolName || ''} onChange={(e) => handleSchoolFormChange(index, e)} />
+                {/* Degree Awarded */}
+                <label  htmlFor='degree'>Degree</label>
+                <input type="text"   placeholder="Masters in React" name="degree" value={data.degree || ''} onChange={(e) => handleSchoolFormChange(index, e)} />
+                {/* Year of Graduation */}
+                <label  htmlFor='date'>Year Completed</label>
+                <input type="date" name="date"  value={data.finisheDate || ''}  onChange={(e) => handleSchoolFormChange(index, e)}/>
+                <label  htmlFor='date'>Year Completed</label>
+                <textarea type="date" name="description"  value={data.description || ''}  onChange={(e) => handleSchoolFormChange(index, e)}/>
 
-        {/* {companyName: '', position: '', dateStarted: '', dateFinished: ''} */}
-        <div>
-          <h2>Professional experience</h2>
+                <div><p>delete</p><button onClick={ () => handleDeleteEducation(index)}>-</button></div>
+              </form>
+            ))}
+          </div>
+          <div><h4>Add more education:</h4><button onClick={handleAddEducation}>+</button></div>
+        </div>
+        
+        <div className="projessional-container form-container">
+          <h4>Professional experience</h4>
           {professionalFormData.map((data, index) => (
-            <div key={index}>
+
+            <form key={index}>
+              {/* Company Name */}
               <label htmlFor="companyName">Company Name</label>
               <input type="text" name="companyName" value={data.companyName || ''} onChange={(e) => handleProfessionalFormDataChange(index, e)} />
-
+            {/* Position */}
               <label htmlFor="companyName">Position</label>
               <input type="text" name="position" value={data.position || ''} onChange={(e) => handleProfessionalFormDataChange(index, e)} />
-              
-    
-
+            {/* Stare Date */}
               <label htmlFor="companyName">Start Date</label>
               <input type="date" name="companyName" value={data.dateStarted || ''} onChange={(e) => handleProfessionalFormDataChange(index, e)}/>
-
+            {/* End Date */}
               <label htmlFor="companyName">End Date</label>
               <input type="date" name="companyName" value={data.dateFinished || ''} onChange={(e) => handleProfessionalFormDataChange(index, e)}/>
-
+            {/* Description of responsibilities */}
               <label htmlFor="companyName">Description</label>
               <textarea type="text" name="companyName" placeholder="describe your responsibilities at work" value={data.description || ''} onChange={(e) => handleProfessionalFormDataChange(index, e)} />
-              
-              <div>delete<button onClick={(e) => handleDeleteProfession(index, e)}>-</button></div>
-            </div>
+            {/* Delete Button */}
+              <div><p>delete</p><button onClick={(e) => handleDeleteProfession(index, e)}>-</button></div>
+            </form>
           ))}
+          <div><h4>Add more work experiences:</h4><button onClick={addProfession}>+</button></div>
         </div>
-        <div><h3>Add more work experiences:</h3><button onClick={addProfession}>+</button></div>
+        
       </div>
-      <div>
+      <div className="resume-container">
         <div>
-          <h3>{info.firstName}</h3>
-          <h3>{info.lastName}</h3>
-          <h3>{info.phone}</h3>
-          <h3>{info.email}</h3>
+          <h5>{info.firstName} | </h5>
+          <h5>{info.lastName} | </h5>
+          <h5>{info.phone} | </h5>
+          <h5>{info.email}</h5>
         </div>
         <div>
           {schoolFormData.map((data, index) => (
             <div key={index}>
-            <div>{data.schoolName}</div>
-            <div>{data.degree}</div>
-            <div>{data.finishDate}</div>
+            <h4>{data.schoolName}</h4>
+            <h4>{data.degree}</h4>
+            <h4>{data.finishDate}</h4>
+            <h4>{data.description}</h4>
             </div>
           ))}
         </div>
